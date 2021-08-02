058246eb6e68f3123c284cf29d27b8523a57e5d9
Manikandan2526 <62885256+Manikandan2526@users.noreply.github.com>
2021-08-02 08:38:04 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://ec2-3-22-234-45.us-east-2.compute.amazonaws.com:3000',
+  apiURL: 'https://stock-interview.herokuapp.com',
   EnDe_Key: {
     Key: '3jEYde6723812djowWfCFtwnmd720JSs', 
     IV: "kI1fhd72362hdalkjfUDnqw237EHzFYe" 
