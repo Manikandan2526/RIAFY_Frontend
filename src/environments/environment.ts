9f102c65293b4a2a7668244057192afacffeda08
Manikandan2526 <62885256+Manikandan2526@users.noreply.github.com>
2021-08-02 08:38:24 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:3000',
+  apiURL: 'https://stock-interview.herokuapp.com',
   EnDe_Key: {
     Key: '3jEYde6723812djowWfCFtwnmd720JSs', 
     IV: "kI1fhd72362hdalkjfUDnqw237EHzFYe" 
